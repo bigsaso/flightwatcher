85cafdc3d9046b7fea1b7c0bdcd9b8aaac039176
bigsaso <sal.logozzo@gmail.com>
2026-01-19 16:00:56 -0500

    Added CORS options

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.flights.router import router as flights_router
 from app.api.rules.router import router as rules_router
 
 app = FastAPI(title="Flights Watcher API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(flights_router)
 app.include_router(rules_router)
